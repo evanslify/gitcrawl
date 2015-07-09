b94c9be48f0b95edbc6e8db020da051623a63cae
evanslify <alpha.bhk@gmail.com>
2015-07-09 13:37:03 +0800

    work

--- fetch/fetch/spiders/git.py
+++ fetch/fetch/spiders/git.py
@@ -1,159 +1,153 @@
+# working :(
+    
 import scrapy
 import json
+import re
 from scrapy.http import Request
 from scrapy.loader import ItemLoader
-import re
-from fetch.items import RepoInfo, UserInfo
-import logging
 
 class GitSpider(scrapy.Spider):
     name = 'git'
 
-    start_urls = ['https://api.github.com/users/Ocramius',
-                  'https://api.github.com/users/michalbe',
-                  'https://api.github.com/users/kevinsawicki',
-                  'https://api.github.com/users/brianchandotcom',
-                  'https://api.github.com/users/fabpot',
-                  'https://api.github.com/users/weierophinney',
-                  'https://api.github.com/users/GrahamCampbell',
-                  'https://api.github.com/users/mrmrs',
-                  'https://api.github.com/users/rkh',
-                  'https://api.github.com/users/md-5']
+    start_urls = ['https://api.github.com/users/xxg1413']
 
     http_user = 'evanslify'
     http_pass = '4e4b57cc169d0e7a6812f73a9a48843b84a2200b'
 
+    def callnext(self, response):
+
+        ''' Call next target for the item loader, or yields it if completed. '''
+        # borrowed from http://goo.gl/OfMLXG
+        # Get the meta object from the request, as the response
+        # does not contain it.
+        meta = response.request.meta
+        # Items remaining in the stack? Execute them
+        if len(meta['callstack']) > 0:
+            target = meta['callstack'].pop(0)
+            yield scrapy.Request(url=target['url'], meta=meta, callback=target['callback'], errback=self.callnext)
+        else:
+            yield response.meta.get('Loader').get('UserInfo')
+
+        if len(meta['callstack']) % 3 == 0:
+            yield response.meta.get('Loader').get('RepoInfo')
+
     def pageturn(self, header):
-        # check whether this is the last page.
-        # logging.info('Pageturn is running.')
+        # called only when response.headers.get('Link') returns valid 
+        # (indicates that this request has multiple pages)
 
         if header.find('next') != -1:
-        # if 'next' is found, this is not the last page
+            # if valid, first url will be the next page
             new_page = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', header)[0][:-2]
-            # long fucking regex
-            # returns the first URL
         else:
             new_page = 'last'
         return str(new_page)
 
-
     def parse(self, response):
 
-        # parse user page.
+        # parsing user page.
+        # https://api.github.com/users/<username>
+
         jr = json.loads(response.body_as_unicode())
-        item = UserInfo()
-        item.update({
+
+        response.meta.update({'Loader': {'UserInfo': {
             'user_public_repo_counts': jr.get('public_repos'),
             'user_hireable': jr.get('hireable'),
             'user_id': jr.get('login'),
             'user_name': jr.get('name'),
             'user_blog': jr.get('blog'),
-            'user_followers_count': jr.get('followers'),
-            })
-
-        yield item
+            'user_followers_count': jr.get('followers')
+            }}})
         new_url = jr.get('repos_url') + '?per_page=100'
-        yield scrapy.Request(new_url, self.parse_all_repos)
-        # go to repos page
+        response.meta.update({'callstack': [{'url': new_url, 'callback': self.parse_all_repos}]})
+        # from pudb import set_trace; set_trace()
+        return self.callnext(response)
         
-        if int(response.headers.get('X-RateLimit-Remaining')) < 100:
-            logging.critical('Remaining ratelimit <100, stopping')
-            return
-
-        # stops while remaining ratelimit <100......
-
     def parse_all_repos(self, response):
-        
-        # parse repo list, extract individual repo link.
-        # a user can have many repos so we create a iterating list
-        # to parse every repo
-        # this may cause some bandwith problems
+        # parsing repository list of a user
+        items = response.meta['Loader']
         jr = json.loads(response.body_as_unicode())
+
         repo_urls = []
-        for i in range(1, len(jr)):
+        repo_urls_to_parse = []
+        response.meta.update({'callstack': []})
+        callstack = response.meta['callstack']
+                
+        for i in range(0, len(jr)):
             repo_urls.append(jr[i]['url'])
-        for url in repo_urls:
-            # yield scrapy.Request(url, self.parse_repo)
-            yield scrapy.Request(url, self.parse_repo)
+
+        for ii in range(0, len(repo_urls)):
+            # repo_urls[ii] = https://api.github.com/repos/<username>/<reponame>
+            callstack.append({'url': repo_urls[ii], 'callback': self.parse_repo})
+            callstack.append({'url': repo_urls[ii] + '/forks?per_page=100', 'callback': self.parse_repo_forks})
+            callstack.append({'url': repo_urls[ii] + '/contributors?per_page=100', 'callback': self.parse_repo_contributors})
+
+        return self.callnext(response)
 
     def parse_repo(self, response):
-        
-        # parse details from a repo.
-
+        # parsing details from a repository
+        items = response.meta['Loader']
         jr = json.loads(response.body_as_unicode())
-        item = RepoInfo()
-        item.update({
+
+        items.update({'RepoInfo': {
             'forks_from': jr.get('fork'),
             'url': jr.get('html_url'),
             'apiurl': jr.get('url'),
             'zipurl': jr.get('html_url') + "/archive/master.zip",
-            })
-        
-        if 'parent' in jr:
-            item.update({
-             'forks_from_url': jr.get('parent').get('url'),
-             })
-        # yield item
-        new_url = jr.get('forks_url') + '?per_page=100'
-        yield item
-        yield scrapy.Request(new_url, self.parse_repo_forks)
-        
+            'forks_from_url': jr.get('parent').get('url') if jr.get('parent') != None else '',
+            }})
 
+        return self.callnext(response)
+        
     def parse_repo_forks(self, response):
     # parse repo forks
     # fork can have many items, github API gives 100 max each page using ?per_page=x
-        
+        items = response.meta['Loader']
         jr = json.loads(response.body_as_unicode())
-        item = RepoInfo()
-        
-        item.update({
-            'forks': item.get('forks', int()) + len(jr)
-            })        
+        fork_usernames = []
 
-        # check whether this repo has fork
         if len(jr) > 1:
-        # then proceed to parse every username who forked this repo.
             for i in range(1, len(jr)):
-                fork_usernames = jr[i].get('owner').get('login')
-                item.update({
-                    'fork_usernames': item.get('fork_usernames', '') + ", " + fork_usernames
-                    })
+                fork_usernames.append(jr[i].get('owner').get('login'))
+
+        old_fork_usernames = items.get('RepoInfo').get('fork_usernames', [])
+
+        items.get('RepoInfo').update({
+            'forks': len(jr) + items.get('RepoInfo').get('forks', 0),
+            'fork_usernames': old_fork_usernames + fork_usernames,
+            })
 
         # implant the method to "turn page" here..
-        turn = ""
+        turn = ''
+        callstack = response.meta['callstack']
 
         if 'Link' in response.headers:
-            # logging.info('sending to pageturn: ' + response.headers.get('Link'))
             turn = self.pageturn(response.headers.get('Link'))
-            # logging.info('now querying url: ' + response.url)
-            # logging.info('got link header: ' + response.headers.get('Link'))
-            # logging.info('pageturn replies: ' + turn)
 
-        if turn != 'last' and turn != "":
-            yield scrapy.Request(turn, self.parse_repo_forks)
-
+        if turn != 'last' and turn != '':
+            callstack.insert(0, {
+                            'url': turn, 'callback': self.parse_repo_forks,
+                            })
+            return self.callnext(response)
         else:
-            yield item
-            # this is a whack way "reverse replace"
-            new_url = 'contributors'.join(response.url.rsplit('forks', 1))
-            yield scrapy.Request(new_url, self.parse_repo_contributors)
+            return self.callnext(response)
 
     def parse_repo_contributors(self, response):
         # parse this repo's count of contributors
+        items = response.meta['Loader']
         jr = json.loads(response.body_as_unicode())
-        item = RepoInfo()
-        item.update({
-        'contributed_times': len(jr),
-        })
+        
+        items.get('RepoInfo').update({
+            'contributed_times': len(jr),
+            })
 
         # check whether this repo has contributors
         if len(jr) > 1:
+            contributed_users = []
             # ... then proceed to parse every username who contributed to this repo.
             for i in range(1, len(jr)):
-                contributed_users = jr[i].get('login')
+                contributed_users.append(jr[i].get('login'))
                 item.update({
-                    'contributed_users': item.get('contributed_users', '') + ", " + contributed_users
+                    'contributed_users': contributed_users,
                     })
-
-        return item
-        return
+        
+        return self.callnext(response)
