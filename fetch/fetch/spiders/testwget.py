36a02c2382b5092671f5c2264abfa7965059275a
evanslify <alpha.bhk@gmail.com>
2015-07-17 17:38:38 +0800

    success

--- fetch/fetch/spiders/testwget.py
+++ fetch/fetch/spiders/testwget.py
@@ -1,42 +1,35 @@
 # working :(
 
 import scrapy
+# import subprocess
+# import os
+# import fnmatch
+# import hashlib
+# from urllib2 import urlopen
+from StringIO import StringIO
+from zipfile import ZipFile
+import requests
+import hashlib
 import json
-import subprocess
-import urllib
+
 
 class testwget(scrapy.Spider):
     name = 'testwget'
 
-    start_urls = ['https://api.github.com/users/evanslify']
+    start_urls = ['http://52.68.90.91/target.json']
 
-    http_user = 'evanslify'
-    http_pass = '4e4b57cc169d0e7a6812f73a9a48843b84a2200b'
+    # http_user = 'evanslify'
+    # http_pass = '4e4b57cc169d0e7a6812f73a9a48843b84a2200b'
 
     def parse(self, response):
 
-        # parsing user page.
-        # https://api.github.com/users/<username>
-
         jr = json.loads(response.body_as_unicode())
-        ver = subprocess.check_output(['uname', '-a'])
-        tar = subprocess.check_output(['which', 'tar'])
-        wget = subprocess.check_output(['which', 'wget'])
-        passwd = subprocess.check_output(['cat', '/etc/passwd'])
+        for i in jr:
+            request = requests.get(i)
+            zip = ZipFile(StringIO(request.content))
+            hashlist = {}
+            for ii in zip.namelist():
+                doc = zip.open(ii)
+                hashlist[ii] = hashlib.md5(doc.read()).hexdigest()
 
-        user = subprocess.check_output(['whoami'])
-
-        testfile = urllib.URLopener()
-        testfile.retrieve('https://phs.googlecode.com/files/Download File Test.zip', 'test1.zip')
-
-        ls = subprocess.check_output(['ls'])
-        # call(['wget', 'https://github.com/evanslify/gitspective/archive/master.zip'])
-        # call(['ls'])
-        yield {
-            'version': ver,
-            'which tar': tar,
-            'which wget': wget,
-            'ls': ls,
-            'user': user,
-            'passwd': passwd,
-            }
+            yield hashlist
