107db158ceeb4a0819906ddd6f812a83f031aa6c
evanslify <alpha.bhk@gmail.com>
2015-07-20 17:36:30 +0800

    fix redis

--- fetch/fetch/pipelines.py
+++ fetch/fetch/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import redis
+import time
 
 
 class FetchPipeline(object):
@@ -14,8 +15,6 @@ class FetchPipeline(object):
 
 
 class RedisPipeline(object):
-
-    collection_name = 'testID'
 
     def __init__(self, redis_uri, redis_port, redis_db, redis_auth):
         self.redis_uri = redis_uri
@@ -37,5 +36,8 @@ class RedisPipeline(object):
         self.r = redis.StrictRedis(connection_pool=self.connection_pool)
 
     def process_item(self, item, spider):
-        self.r.set(self.collection_name, item)
+        epoch = str(int(time.time()))
+        user_login = item.get('UserInfo').get('user_id')
+        key_name = user_login + ";" + epoch
+        self.r.hset('gitcrawl', key_name, item)
         return item
